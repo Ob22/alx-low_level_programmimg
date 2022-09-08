43d3e4aa60d8c123f06909603c82465a2169703b
Ob22 <ehi_glosin77@yahoo.com>
2022-09-08 09:12:46 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 #include <unistd.h>
 
 /**
- * main - Prints out the last part of a quote in the standard error
+ * main - Prints out the last part of a quote in the standard error.
  *
- * Return: 1 if success
+ * Return: 1 if success.
  */
 int main(void)
 {
