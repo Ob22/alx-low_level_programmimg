a8c9e0354a8e6f4b429b6ee7f99ffbea271979f9
Ob22 <ehi_glosin77@yahoo.com>
2022-09-14 04:14:54 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* main - Prints a random number and stated whether
+* it is positive, negative or zero
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	int i
+		
+		if (i > 0)
+			_putchar("%i is positive\n", n);
+		else if (n < 0)
+			_putchar("%i is negative\n", n);
+
+		else
+			_putchar("%i is zero\n", n);
+
+	return (0);
+}
