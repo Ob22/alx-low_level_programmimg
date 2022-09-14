14ddbbfa48b04fd86e9cb4a089d92fc84e17c6b9
Ob22 <ehi_glosin77@yahoo.com>
2022-09-14 08:54:49 -0700

    Tas 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,24 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
-
-if (a > b && b > c)
-{
-	largest = a;
-}
-else if (b > a && a > c)
-{
-	largest = b;
-}
-else if (c > b)
-{
-	largest = c;
-}
-else
-{
-	largest = b;
-}
-
-return (largest);
+	int a, b, c;
+	
+	if (a > b && b > c)
+	{
+		return (largest = a);
+	}
+	else if (b > a && a > c)
+	{
+		return (largest = b);
+	}
+	else
+	{
+		return (largest = c);
+	}
+	
+	return (largest);
 }
