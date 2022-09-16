927e35d674676991f5f8b26ff7510b1237c344b1
Ob22 <ehi_glosin77@yahoo.com>
2022-09-16 14:24:48 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,28 +5,29 @@
 * print Fizz instead of the number, for multiples of five print Buzz
 * and for multiples of both five and three print FizzBuzz
 *
-* Return:Always 0
+* Return:Always 0 (Success)
 */
 int main(void)
 {
-	int num;
-
-	for (num = 1; num <= 100; num++)
+	int a;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
+	
+	for (a = 1; a <= 100; a++)
 	{
-		if ((num % 3) == 0 && (num % 5) == 0)
-			printf("FizzBuzz");
-		else
-			if ((num % 3) == 0)
-				printf("Fizz");
-			else
-				if ((num % 5) == 0)
-					printf("Buzz);
+		if (a == 100)
+			printf("%s", b);
+		else if ((a % 3 == 0) && (a % 5 == 0)
+				printf("%s ", fb);
+				else if (a % 3 == 0)
+				printf("%s ", f);
+				else if (a % 5 == 0)
+				printf("%s ", b);
 				else
-					printf("%d", num);
-		if (num == 100)
-			continue;
-		printf(" ");
-	}
-	printf("\n");
-	return (0);
-}
+				printf("%d ", a);
+				}
+				printf("\n");
+				return (0);
+				}
+
