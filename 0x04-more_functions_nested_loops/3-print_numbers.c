cf1e440392de4eadb457fb191743a3acfde6ce8d
Ob22 <ehi_glosin77@yahoo.com>
2022-09-16 05:17:26 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,16 +3,15 @@
 /**
 * print_numbers - prints the numbers from 0 to 9
 * followed by a new line
-* Return: Always 0.
 */
 void print_numbers(void)
 {
-	int num = 0;
+	int a = 0;
 
-	while (num <= 9)
+	while (a <= 9)
 	{
-		_putchar('0' + num);
-		c++
+		_putchar('0' + a);
+		a++
 	}
 	_putchar('\n');
 }
