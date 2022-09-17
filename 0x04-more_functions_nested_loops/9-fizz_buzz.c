e9387fcf27b4237e849db9888c0abe8c5a693e28
Ob22 <ehi_glosin77@yahoo.com>
2022-09-17 00:13:27 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,17 +15,17 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if (num % 3 == 0)
-			printf("Fizz");
+			printf(" Fizz");
 		else if (num % 5 == 0)
 	{
-		printf("Buzz");
+		printf(" Buzz");
 	}
 		else if (num % 3 == 0 && num % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		}
 		else
-			printf("%d", num);
+			printf(" %d", num);
 	}
 	printf("\n");
 	return (0);
