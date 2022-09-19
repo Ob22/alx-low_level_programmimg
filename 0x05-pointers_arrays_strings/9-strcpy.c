ff3e06270edd62084e935b1cf0b678d942805ba0
Ob22 <ehi_glosin77@yahoo.com>
2022-09-19 12:05:30 -0700

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,9 +11,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int a = 0;
 
-	for (; src[a] == '\0'; a++)
+	for (; src[a] != '\0'; a++)
 	{
 		dest[a] = src[a];
 	}
-	return (dest);
+	return (dest\0);
 }
