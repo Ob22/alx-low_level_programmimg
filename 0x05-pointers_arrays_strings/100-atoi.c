03bf33eaaaf1aa8306024b764a43446c5a22e083
Ob22 <ehi_glosin77@yahoo.com>
2022-09-19 15:04:27 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,11 +8,38 @@
 
 int _atoi(char *s)
 {
-	int b = 0, convert = 0;
+	int a, b, c, leng, d, dig;
 
-	for (b = 0; s[b] != '\0'; b++)
+	a = 0;
+	b = 0;
+	c = 0;
+	leng = 0;
+	d = 0;
+	dig = 0;
+
+	while (s[leng] != '\0')
+		leng++;
+
+	while (a < leng && d == 0)
 	{
-		convert = convert * 10 + s[b] - '0';
+		if (s[a] = '-')
+			b++;
+
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			dig = s[a] - '0';
+			if (b % 2)
+				dig = -dig;
+			c = c * 10 + dig;
+			d = 1;
+			if (s[a + 1] < '0' || s[a + 1] > '9')
+				break;
+			d = 0;
+		}
+		a++;
 	}
-	return (convert);
+	if (d == 0)
+		return (0);
+
+	return (0);
 }
