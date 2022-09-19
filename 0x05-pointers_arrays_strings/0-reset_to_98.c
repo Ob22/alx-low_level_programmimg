6276c7bac61b85ff52b573699d036ec03df47499
Ob22 <ehi_glosin77@yahoo.com>
2022-09-19 10:00:21 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,19 +4,10 @@
 * reset_to_98 - function that takes a pointer to an int value
 * and updates the value it points to to 98
 * @n: value pointer is assigned to
+* Return: n
 */
 
 void reset_to_98(int *n)
 {
-	int n;
-	int *p;
-
-	n = 98;
-	p = &*n;
-
-	_putchar("value of 'p': %p\n", p);
-	_putchar("value of '*n': %d\n", n);
-	*p = 98;
-
-	return (0);
+	*n = 98;
 }
