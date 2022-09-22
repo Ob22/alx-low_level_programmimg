f8d6994afc414ade5bc7f774f84f1ad5fcb3ae2f
Ob22 <ehi_glosin77@yahoo.com>
2022-09-22 11:06:00 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* string_toupper - changes all lowercase letters
+* of a string to uppercase
+* @r: input string
+* Return: pointer
+*/
+
+char *string_toupper(char *r)
+{
+	int count = 0;
+
+	while (*(r + count) != '\0')
+	{
+		if ((*(r + count) > 97) && (*(r + count) <= 122))
+			*(r + count) = *(r + count) - 32;
+		count++;
+	}
+	return (r)
+}
