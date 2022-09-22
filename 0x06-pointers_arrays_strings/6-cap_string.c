761403d927aaea971c729fa2b4415cad3f6baead
Ob22 <ehi_glosin77@yahoo.com>
2022-09-22 12:22:59 -0700

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* cap_string - capitalized all words of a string
+* @r: input string
+* Return: caps of words
+*/
+
+char *cap_string(char *r)
+{
+	int a, b;
+	int cap = 32;
+	int separators[] = {',', ';', '.', '?', '"', '!', '(', ')', ' ', '{', '}', '\n', '\t'};
+
+	for (a = 0; r[a] != '\0'; a++)
+	{
+		if (r[a] >= 'a' && r[a] <= 'z')
+		{
+			r[a] = r[a] - cap;
+		}
+		cap = 0;
+		for (b = 0; b <= 12; b++)
+		{
+			if (r[a] == separators[b])
+			{
+				b = 12;
+				cap = 32;
+			}
+		}
+	}
+	return (r);
+}
