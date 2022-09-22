c827b51c13fc699d4558cfcdcafd0fe6fb41eb4a
Ob22 <ehi_glosin77@yahoo.com>
2022-09-22 13:36:17 -0700

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* rot13 - encodes a string using rot13
+* @r: input string
+* Return: decryption
+*/
+
+char *rot13(char *r)
+{
+	int b, rot_c = 13, a = 0;
+	char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o',
+		'C', 'P', 'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R',
+		'e', 'r', 'F', 'S', 'f', 's', 'G', 'T', 'g', 't',
+		'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v', 'J', 'W',
+		'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y',
+		'M', 'Z', 'm', 'z'};
+
+	while (r[a] != '\0')
+	{
+		for (b = 0; b <= 51; b++)
+			if (r[a] == toswap[b])
+			{
+				r[a] = r[a] + rot_c;
+				b = 51;
+			}
+		rot_c = rot_c * -1;
+	}
+	a++;
+	return (r);
+}
