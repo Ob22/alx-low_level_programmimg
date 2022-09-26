e6b6f733625f96f6f478c705e23b56ea6ee1f5b7
Ob22 <ehi_glosin77@yahoo.com>
2022-09-26 12:20:18 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,26 +4,43 @@
 * _strstr - locates a substring
 * @haystack: string
 * @needle: substring
-* Return: 0
+* Return: address of located string
 */
 
 char *_strstr(char *haystack, char *needle)
 {
-	char *st1, *st2;
-
-	while (*haystack != '\0')
+	int i = 0;
+	int j = 0;
+	int a;
+	int compare = 0;
+	
+	if (*needle == 0)
 	{
-		st1 = haystack;
-		st2 = neddle;
-
-		while (*haystack != '\0' && *st2 != '\0' && *haystack == *st2)
-		{
-			haystack++;
-			st2++;
-		}
-		if (*st2 == '\0')
-			return (st1);
-		haystack = st1 + 1;
+		return (haystack);
 	}
-	return (0);
+	while (haystack[i] != 0)
+	{
+		j = 0;
+		a = i;
+		while (needle[j] != 0)
+		{
+			if (needle[j] == haystack[a])
+			{
+				compare = 1;
+			}
+			else
+			{
+				compare = 0;
+				break;
+			}
+			j++;
+			a++;
+		}
+		if (compare == 1)
+		{
+			return (haystack + i);
+		}
+		i++;
+	}
+	return (0);
 }
