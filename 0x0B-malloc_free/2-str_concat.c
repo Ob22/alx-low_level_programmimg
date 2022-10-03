7ccc997c749944eaf328a6f6defd7a656bfff9f7
Ob22 <ehi_glosin77@yahoo.com>
2022-10-03 08:18:00 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,8 +15,8 @@ char *str_concat(char *s1, char *s2)
 	unsigned int len1, len2, size, a, b;
 	char *pstr;
 	
-	if (s1 == NULL)
-		s1 = "";
+	if (s1 == NULL)
+		s1 = "";
 	if (s2 == NULL)
                 s2 = "";
 
